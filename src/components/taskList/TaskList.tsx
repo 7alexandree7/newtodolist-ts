8ef2694439f530549a518ce4dc78c7370f48fb39
alexandre <alexandre020602@gmail.com>
2025-07-02 12:46:09 -0300

    recebendo do componente pai e listando + ajustes de estilo + recebendo tipagem via props da interface

--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -1,11 +1,22 @@
 //import React from 'react'
+import type TaskType from '../../interfaces/Tasks/Tasks'
 
-const TaskList = () => {
-  return (
-    <div>
-      tasklist
-    </div>
-  )
+interface TasklistProps {
+    tasks: TaskType[]
+}
+
+const TaskList = ({ tasks }: TasklistProps) => {
+
+    console.log(tasks)
+
+    return (
+        <ul className='space-y-4 mt-4'>
+            {tasks.map((task) => (
+                <li key={task.id} className='bg-slate-400 text-white p-2 rounded-md'>{task.title}</li>
+
+            ))}
+        </ul>
+    )
 }
 
 export default TaskList
