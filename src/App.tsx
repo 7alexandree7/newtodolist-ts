5d63c66f594a1320b5664ed1967a8988bb0dc71c
alexandre <alexandre020602@gmail.com>
2025-07-02 12:00:24 -0300

    criando uma interface externa para minhas task, e iniciando com valores mokados

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import AddTasks from './components/AddTasks/AddTasks'
 import TaskList from './components/taskList/TaskList'
+import type TaskType from './interfaces/Tasks/Tasks'
+
 
 function App() {
 
+  const [tasks, setTasks] = useState<TaskType[]>([
+
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Estudar React para melhorar habilidades de desenvolvimento web.",
+      isCompleted: false
+    },
+
+    {
+      id: 2,
+      title: "Fazer compras",
+      description: "Comprar frutas, verduras e laticínios.",
+      isCompleted: false
+    },
+
+    {
+      id: 3,
+      title: "Ler um livro",
+      description: "Ler pelo menos 30 páginas do livro atual.",
+      isCompleted: false
+    },
+  ])
+
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTasks />
-      <TaskList />
+    <div className='bg-slate-500 w-screen h-screen flex flex-col items-center p-6'>
+      <div className='w-[500px]'>
+        <h1 className='text-3xl text-slate-100 font-bold text-center'>Gerenciador de Tarefas</h1>
+        <AddTasks />
+        <TaskList />
+      </div>
+
     </div>
   )
 }
 
-export default App
+export default App  
